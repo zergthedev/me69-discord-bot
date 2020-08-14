1850dc7d9c065aa9670db8365fed477e155ef433
zergthedev <60578485+zergthedev@users.noreply.github.com>
2020-08-14 22:00:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '//';
-
+ 
+const prefix = '//'; //prefix
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js')); //read directories for commands (./commands/)
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
 client.once('ready', () => {
-	console.log('Bot is online!');
+    console.log('ME69 is online!'); //logon
 });
-
-client.on('message', message =>{
-	if(!message.content.startsWith(prefix) || message.author.bot) return;
-	
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command =  args.shift().toLowerCase();
-	
-	if(command === 'test'){
-		if(message.member.roles.cache.has('739406106866221057')){
-			message.channel.send('<:info95:739833428782088214>');
-			message.channel.send('Fully works!');
-		} else  {
-			message.channel.send('Sorry, You dont have enough permissions to use this command.')
-		}
-	} else if (command == 'help'){
-		message.channel.send('<:question95:739831834673938443>');
-		message.channel.send('Making...')
+ 
+client.on('message', message =>{ // commands
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'help'){
+        client.commands.get('help').execute(message, args); //help command
+    } else if (command === 'test'){
+		client.commands.get('test').execute(message, args); //test command
+	} else if (command === 'github'){
+		client.commands.get('github').execute(message, args); //github command
 	}
 });
 
-client.login('token')
+client.login('') //insert your token here
